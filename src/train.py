d9c73d745c0264536e6357bf4a16f2bf4cc20dea
Ketil Malde <ketil@malde.org>
2020-05-07 20:14:03 +0200

    Clean up train.py, factor out class_names and subdirs, rm commented

--- src/train.py
+++ src/train.py
@@ -23,20 +23,25 @@ class DeepVisionConfig(Config):
     STEPS_PER_EPOCH = 131
     # more parameters...
 
+class_names = ['BG','bluewhiting','herring','lanternfish','mackerel']
+    
 # class that defines and loads the data set
 class DeepVisionDataset(Dataset):
 
-    def load_dataset(self, dataset_dir, is_train=True):
-         self.add_class("dataset", 1, "bluewhiting")
-         self.add_class("dataset", 2, "herring")         
-         self.add_class("dataset", 3, "lanternfish")
-         self.add_class("dataset", 4, "mackerel")
-         
-         images_dirs = [os.path.join(dataset_dir,y) for y in ['sim-2017', 'sim-2018']]
+    def load_dataset(self, dataset_dirs, is_train=True):
+         for i,n in enumerate(class_names[1:]):
+             self.add_class("dataset", i, n)
+
+         if isinstance(dataset_dirs, str):
+             images_dirs = [dataset_dirs]
+         elif isinstance(dataset_dirs, list):
+             images_dirs = dataset_dirs
+         else:
+             error("'dataset_dirs' must be a list or a string")
 
          for images_dir in images_dirs:
              filenames = [f for f in os.listdir(images_dir) if re.match(r'sim-201[78]_[0-9]+\.png', f)]
-             print("*** Number of images seen: ", len(filenames), "***")
+             print("*** Directory: ",images_dir,"Number of images seen: ", len(filenames), "***")
              for filename in filenames:
                  image_id = filename[:-4]  # skip .png suffix
                  # skip all images after 150 if we are building the train set
@@ -48,28 +53,6 @@ class DeepVisionDataset(Dataset):
                  img_path = os.path.join(images_dir,filename)
                  ann_path = os.path.join(images_dir,image_id + '.txt')
                  self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
-
-    # extract bounding boxes from an annotation file (used by load_mask below)
-    # this function just constructs a rectangle corresponding to the bbox!
-    # parses the Kangaroo xml file to get data
-    # def extract_boxes(self, filename):
-    #     # load and parse the file
-    #     tree = ElementTree.parse(filename)
-    #     # get the root of the document
-    #     root = tree.getroot()
-    #     # extract each bounding box
-    #     boxes = list()
-    #     for box in root.findall('.//bndbox'):
-    #         xmin = int(box.find('xmin').text)
-    #         ymin = int(box.find('ymin').text)
-    #         xmax = int(box.find('xmax').text)
-    #         ymax = int(box.find('ymax').text)
-    #         coors = [xmin, ymin, xmax, ymax]
-    #         boxes.append(coors)
-    #     # extract image dimensions
-    #     width = int(root.find('.//size/width').text)
-    #     height = int(root.find('.//size/height').text)
-    #     return boxes, width, height
 
     # load the masks for an image
     def load_mask(self, image_id):
@@ -99,15 +82,17 @@ class DeepVisionDataset(Dataset):
         info = self.image_info[image_id]
         return info['path']
 
+subdirs = ['sim-2017', 'sim-2018']
+
 # prepare train set
 train_set = DeepVisionDataset()
-train_set.load_dataset('/data', is_train=True)
+train_set.load_dataset([os.path.join('/data',y) for y in subdirs], is_train=True)
 train_set.prepare()
 print('Train: %d' % len(train_set.image_ids))
 
 # prepare test/val set
 test_set = DeepVisionDataset()
-test_set.load_dataset('/data/validation', is_train=False)
+test_set.load_dataset([os.path.join('/data/validation',y) for y in subdirs], is_train=False)
 test_set.prepare()
 
 print('Test: %d' % len(test_set.image_ids))
@@ -115,10 +100,8 @@ print('Test: %d' % len(test_set.image_ids))
 config = DeepVisionConfig()
 config.display()
 
-# define the model
+# define the model, load weights and run training
 model = MaskRCNN(mode='training', model_dir='./', config=config)
-# load weights (mscoco) and exclude the output layers
 model.load_weights('mask_rcnn_coco.h5', by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
-# train weights (output layers or 'heads')
 model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
 
