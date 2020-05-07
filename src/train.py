d3f4985f85af1f3e327e63c5632480814b147856
Ketil Malde <ketil.malde@imr.no>
2020-05-07 13:21:48 +0200

    Iterate over separate 2017, 2018 dirs

--- src/train.py
+++ src/train.py
@@ -32,22 +32,22 @@ class DeepVisionDataset(Dataset):
          self.add_class("dataset", 3, "lanternfish")
          self.add_class("dataset", 4, "mackerel")
          
-         images_dir = dataset_dir       # + '/images/'
-         annotations_dir = dataset_dir  # + '/annots/'
+         images_dirs = [os.path.join(dataset_dir,y) for y in ['sim-2017', 'sim-2018']]
 
-         filenames = [f for f in os.listdir(images_dir) if re.match(r'simulated_images_[0-9]+\.png', f)]
-         print("*** Number of images seen: ", len(filenames), "***")
-         for filename in filenames:
-             image_id = filename[:-4]  # skip .png suffix
-             # skip all images after 150 if we are building the train set
-             # if is_train and int(image_id) >= 150:
-             #    continue
-             # skip all images before 150 if we are building the test/val set
-             # if not is_train and int(image_id) < 150:
-             #    continue
-             img_path = os.path.join(images_dir,filename)
-             ann_path = os.path.join(annotations_dir,image_id + '.txt')
-             self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+         for images_dir in images_dirs:
+             filenames = [f for f in os.listdir(images_dir) if re.match(r'sim-201[78]_[0-9]+\.png', f)]
+             print("*** Number of images seen: ", len(filenames), "***")
+             for filename in filenames:
+                 image_id = filename[:-4]  # skip .png suffix
+                 # skip all images after 150 if we are building the train set
+                 # if is_train and int(image_id) >= 150:
+                 #    continue
+                 # skip all images before 150 if we are building the test/val set
+                 # if not is_train and int(image_id) < 150:
+                 #    continue
+                 img_path = os.path.join(images_dir,filename)
+                 ann_path = os.path.join(images_dir,image_id + '.txt')
+                 self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
 
     # extract bounding boxes from an annotation file (used by load_mask below)
     # this function just constructs a rectangle corresponding to the bbox!
@@ -101,13 +101,13 @@ class DeepVisionDataset(Dataset):
 
 # prepare train set
 train_set = DeepVisionDataset()
-train_set.load_dataset('simulated_images', is_train=True)
+train_set.load_dataset('/data', is_train=True)
 train_set.prepare()
 print('Train: %d' % len(train_set.image_ids))
 
 # prepare test/val set
 test_set = DeepVisionDataset()
-test_set.load_dataset('simulated_images', is_train=False)
+test_set.load_dataset('/data', is_train=False)
 test_set.prepare()
 
 print('Test: %d' % len(test_set.image_ids))
