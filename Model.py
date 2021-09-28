4b95794be488786fcbd36f0ee89ef3f13662ca94
Ketil Malde <ketil@malde.org>
2021-09-28 15:14:22 +0200

    Wrap everything in Model class

--- Model.py
+++ Model.py
@@ -14,40 +14,42 @@ GROUPID=os.getgid()
 USERNAME=pwd.getpwuid(USERID).pw_name
 RUNTIME='' # '--gpus device=0'
 
-print(USERID, GROUPID, USERNAME)
+# print(USERID, GROUPID, USERNAME)
 
-def docker_run(args=''):
-    os.system(f'docker run {RUNTIME} --rm --user {USERID}:{GROUPID} -v {DATAIN}:/data:ro -v {DATAOUT}:/project -it {USERNAME}-{IMAGENAME} {args}')
+class Model:
 
-def docker_build(args):
-    os.system(f'docker build --build-arg user={USERNAME} --build-arg uid={USERID} --build-arg gid={GROUPID} -t {USERNAME}-{IMAGENAME} .')
+    def docker_run(args=''):
+        os.system(f'docker run {RUNTIME} --rm --user {USERID}:{GROUPID} -v {DATAIN}:/data:ro -v {DATAOUT}:/project -it {USERNAME}-{IMAGENAME} {args}')
 
-def train():
-    '''Train the network'''
-    # if no initial weights: python3 /src/download_weights.py
-    docker_run('python3 /src/train.py')
+    def docker_build(args):
+        os.system(f'docker build --build-arg user={USERNAME} --build-arg uid={USERID} --build-arg gid={GROUPID} -t {USERNAME}-{IMAGENAME} .')
 
-def check():
-    '''Verify that data is in place and that the output doesn't exist'''
-    pass
+    def train():
+        '''Train the network'''
+        # if no initial weights: python3 /src/download_weights.py
+        docker_run('python3 /src/train.py')
 
-def predict(target, output):
-    '''Run a trained network on the data in target'''
-    pass
+    def check():
+        '''Verify that data is in place and that the output doesn't exist'''
+        pass
 
-def test():
-    '''Run tests'''
-    docker_run('python3 /src/test.py')
-    pass
+    def predict(target, output):
+        '''Run a trained network on the data in target'''
+        pass
 
-def status():
-    '''Print the current training status'''
-    # check if docker exists
-    # check if network is trained (and validation accuracy?)
-    # check if data is present for training
-    # check if test data is present
-    # check if test output is present (and report)
-    pass
+    def test():
+        '''Run tests'''
+        docker_run('python3 /src/test.py')
+        pass
+
+    def status():
+        '''Print the current training status'''
+        # check if docker exists
+        # check if network is trained (and validation accuracy?)
+        # check if data is present for training
+        # check if test data is present
+        # check if test output is present (and report)
+        pass
 
 if __name__ == '__main__':
     import argparse
